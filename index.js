e0628c6dc136b997d2e24cee4c144ab885e86d01
Yimura <andreas.maerten@scarlet.be>
2020-10-14 21:01:37 +0200

    Initial Commit

---
+++ index.js
@@ -0,0 +1,135 @@
+import ServerModule from './structures/ServerModule.js'
+import Constants, { DJMode, RevokeTime } from './util/Constants.js'
+import DJUser from './structures/dj/User.js'
+import { GuildMember } from 'discord.js'
+
+export default class DJSystem extends ServerModule {
+    _cache = new Map();
+
+    /**
+     * @param {Main} main
+     * @param {Guild} server
+     */
+    constructor(main, server) {
+        super(main, server);
+
+        this.register(DJSystem, {
+            name: 'dj',
+            scope: 'server',
+            requires: [
+                'music'
+            ]
+        });
+    }
+
+    get constants() {
+        return Constants;
+    }
+
+    /**
+     * @param {GuildMember}
+     */
+    add(serverMember) {
+        if (this.mode != DJMode['MANAGED']) return;
+
+        this.users.set(serverMember.id, new DJUser(this, serverMember));
+    }
+
+    /**
+     * @param {GuildMemberResolvable} guildMemberResolvable
+     */
+    has(guildMemberResolvable) {
+        if (this.mode != DJMode['MANAGED']) return true;
+
+        const serverMemberId = guildMemberResolvable instanceof GuildMember ? guildMemberResolvable.id : guildMemberResolvable;
+
+        return this.users.has(serverMemberId);
+    }
+
+    /**
+     * @param {GuildMember}
+     */
+    join(serverMember) {
+        if (this.mode != DJMode['MANAGED']) return;
+
+        const djUser = this.users.get(serverMember.id);
+
+        if (djUser) {
+            djUser.clear();
+        }
+    }
+
+    /**
+     * @param {boolean} [hard=false]
+     */
+    reset(hard = false) {
+        if (hard) this.mode = this.getModule('guildSetting').get(this.server.id, 'dj_mode') || DJMode['FREEFORALL'];
+        this.playlistLock = false;
+
+        if (!this.users)
+            this.users = new Map();
+
+        this.users.forEach((djUser) => {
+            djUser.clear();
+
+            this.users.delete(djUser.id);
+        });
+    }
+
+    /**
+     * @param {GuildMember}
+     */
+    remove(serverMember) {
+        if (this.mode != DJMode['MANAGED']) return;
+
+        const djUser = this.users.get(serverMember.id);
+
+        if (djUser && this.size == 1) {
+            djUser.revokeDelay(this.revokeTime);
+        }
+    }
+
+    /**
+     * @param {GuildMember}
+     */
+    resign(serverMember) {
+        const djUser = this.users.get(serverMember.id);
+        djUser.clear();
+
+        if (djUser && this.size == 1) {
+            this.setMode(DJMode['FREEFORALL']);
+
+            this.music.channel.send(`${serverMember} has resigned as DJ, all users in voice channel can now use music commands.`);
+
+            return;
+        }
+
+        this.users.delete(djUser.id);
+    }
+
+    /**
+     * @param {number} mode
+     * @param {boolean} persist
+     */
+    setMode(mode, persist = false) {
+        if (persist) {
+            this.server.options.update('djMode', mode);
+
+            this.getModule('guildSetting').set(this.server.id, 'dj_mode', mode);
+        }
+
+        this.mode = mode;
+
+        this.reset();
+    }
+
+    /**
+     * This method is only ran once to setup the module
+     * we can abuse this to add our event listeners globally
+     */
+    setup() {
+
+
+        return true;
+    }
+}
---
+++ util/Constants.js
@@ -0,0 +1,12 @@
+export const DJMode = {
+    FREEFORALL: 1,
+    MANAGED: 2,
+    ROLE: 3
+};
+
+export const RevokeTime = 12e4;
+
+export default {
+    DJMode,
+    RevokeTime
+};
